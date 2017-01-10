c7fb398e55383862e3fdae772cb24cad771c2670
speedchandu <chandan.bhagat@outlook.com>
2017-01-10 09:07:45 +0545

    Generic stack implemented and queue implemented

--- ClassLibraryTest/Computer/Stack.cs
+++ ClassLibraryTest/Computer/Stack.cs
@@ -80,4 +80,96 @@ namespace ClassLibraryTest.Computer
             return str;
         }
     }
-}
+
+    public class GenericStack<T>
+    {
+        private T[] _container = new T[0];
+
+        public int Count { get { return _container.Length; } }
+        public void Push(T a)
+        {
+            Array.Resize(ref _container, _container.Length + 1);
+            _container[_container.Length - 1] = a;
+        }
+        T ret;
+        public T Pop()
+        {
+
+            if (_container.Length >= 0)
+            {
+                 ret = _container[_container.Length - 1];
+                Array.Resize(ref _container, _container.Length - 1);
+            }
+
+            return ret;
+        }
+
+        public string DisplayAll()
+        {
+            string str = string.Join(",", _container);
+            return str;
+        }
+
+
+    }
+
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Roll { get; set; }
+
+
+        public override string ToString()
+        {
+            string str = $"Id : {this.Id} Roll : {this.Roll} Name : {this.Name}";
+            return str;
+        }
+
+    }
+
+    public class Queuefirst
+    {
+        private int[] _container = new int[10];
+        private int count = 0;
+        public int Count { get { return count; } }
+        public void Enqueue(int a)
+        {
+            if (count<10 && count>=0)
+            {
+                for (int i = count;  i > 0 ;  i--)
+                {
+                    _container[i] = _container[i - 1];
+                }
+                _container[0] = a;
+                count++;
+            }
+        }
+
+
+        public int Dequque()
+        {
+            int ret = 0;
+            if (count>0)
+            {
+                ret=_container[count - 1];
+                _container[count - 1] = 0;
+                count--;
+            }
+
+            return ret;
+        }
+
+        public string DisplayAll()
+        {
+
+            int[] dest = new int[count];
+            Array.Copy(_container, dest, count);
+            string str = string.Join(",", dest);
+            return str;
+        }
+    }
+
+
+    }
--- Consoleapp/Program.cs
+++ Consoleapp/Program.cs
@@ -26,22 +26,88 @@ namespace Consoleapp
             //r1.Breadth = 12;
             //Console.WriteLine($"Area : {r1.Area}\nPerimeter : {r1.Perimeter}");
 
-            c1.CustomStack stack = new c1.CustomStack();
+            //c1.CustomStack stack = new c1.CustomStack();
 
-            Console.WriteLine($"Number of items : {stack.Count}, TotalItems : {stack.DisplayAll()}");
+            //Console.WriteLine($"Number of items : {stack.Count}, TotalItems : {stack.DisplayAll()}");
 
-            stack.Push(1);
-            Console.WriteLine($"Number of items : {stack.Count}, TotalItems : {stack.DisplayAll()}");
-            stack.Push(10);
-            Console.WriteLine($"Number of items : {stack.Count}, TotalItems : {stack.DisplayAll()}");
-            stack.Push(14);
-            stack.Push(3);
-            stack.Push(2);
-            stack.Push(1);
-            Console.WriteLine($"Number of items : {stack.Count}, TotalItems : {stack.DisplayAll()}");
-            stack.Pop();
-            Console.WriteLine($"Number of items : {stack.Count}, TotalItems : {stack.DisplayAll()}");
+            //stack.Push(1);
+            //Console.WriteLine($"Number of items : {stack.Count}, TotalItems : {stack.DisplayAll()}");
+            //stack.Push(10);
+            //Console.WriteLine($"Number of items : {stack.Count}, TotalItems : {stack.DisplayAll()}");
+            //stack.Push(14);
+            //stack.Push(3);
+            //stack.Push(2);
+            //stack.Push(1);
+            //Console.WriteLine($"Number of items : {stack.Count}, TotalItems : {stack.DisplayAll()}");
+            //stack.Pop();
+            //Console.WriteLine($"Number of items : {stack.Count}, TotalItems : {stack.DisplayAll()}");
 
+
+
+            ////testing with generic stack
+            //c1.GenericStack<int> s1 = new c1.GenericStack<int>();
+            //Console.WriteLine($"Number of items : {s1.Count}, TotalItems : {s1.DisplayAll()}");
+
+            //s1.Push(1);
+            //Console.WriteLine($"Number of items : {s1.Count}, TotalItems : {s1.DisplayAll()}");
+            //s1.Push(10);
+            //Console.WriteLine($"Number of items : {s1.Count}, TotalItems : {s1.DisplayAll()}");
+            //s1.Push(14);
+            //s1.Push(3);
+            //s1.Push(2);
+            //s1.Push(1);
+            //Console.WriteLine($"Number of items : {s1.Count}, TotalItems : {s1.DisplayAll()}");
+            //s1.Pop();
+            //Console.WriteLine($"Number of items : {s1.Count}, TotalItems : {s1.DisplayAll()}");
+
+
+            //c1.GenericStack<string> s2  = new c1.GenericStack<string>();
+            //Console.WriteLine($"Number of items : {s2.Count}, TotalItems : {s2.DisplayAll()}");
+
+            //s2.Push("a");
+            //Console.WriteLine($"Number of items : {s2.Count}, TotalItems : {s2.DisplayAll()}");
+            //s2.Push("b");
+            //Console.WriteLine($"Number of items : {s2.Count}, TotalItems : {s2.DisplayAll()}");
+            //s2.Push("c");
+            //s2.Push("d");
+            //s2.Push("e");
+            //s2.Push("f");
+            //Console.WriteLine($"Number of items : {s2.Count}, TotalItems : {s2.DisplayAll()}");
+            //s2.Pop();
+            //Console.WriteLine($"Number of items : {s2.Count}, TotalItems : {s2.DisplayAll()}");
+
+            //c1.GenericStack<c1.Student> s3  = new c1.GenericStack<c1.Student>();
+            //Console.WriteLine($"Number of items : {s3.Count}, TotalItems : {s3.DisplayAll()}");
+
+            //s3.Push(new c1.Student() {  Id=1, Name="a", Roll="a1"});
+            //Console.WriteLine($"Number of items : {s3.Count}, TotalItems : {s3.DisplayAll()}");
+            //s3.Push(new c1.Student() { Id = 2, Name = "b", Roll = "b2" });
+            //Console.WriteLine($"Number of items : {s3.Count}, TotalItems : {s3.DisplayAll()}");
+            //s3.Push(new c1.Student() { Id = 3, Name = "c", Roll = "c3" });
+            //s3.Push(new c1.Student() { Id = 4, Name = "d", Roll = "d4" });
+            //s3.Push(new c1.Student() { Id = 5, Name = "e", Roll = "e5" });
+            //s3.Push(new c1.Student() { Id = 6, Name = "f", Roll = "f6" });
+            //Console.WriteLine($"Number of items : {s3.Count}, TotalItems : {s3.DisplayAll()}");
+            //s3.Pop();
+            //Console.WriteLine($"Number of items : {s3.Count}, TotalItems : {s3.DisplayAll()}");
+
+
+
+            //queue implemented
+            c1.Queuefirst s1 = new ClassLibraryTest.Computer.Queuefirst();
+            Console.WriteLine($"Number of items : {s1.Count}, TotalItems : {s1.DisplayAll()}");
+
+            s1.Enqueue(1);
+            Console.WriteLine($"Number of items : {s1.Count}, TotalItems : {s1.DisplayAll()}");
+            s1.Enqueue(10);
+            Console.WriteLine($"Number of items : {s1.Count}, TotalItems : {s1.DisplayAll()}");
+            s1.Enqueue(14);
+            s1.Enqueue(3);
+            s1.Enqueue(2);
+            s1.Enqueue(1);
+            Console.WriteLine($"Number of items : {s1.Count}, TotalItems : {s1.DisplayAll()}");
+            s1.Dequque();
+            Console.WriteLine($"Number of items : {s1.Count}, TotalItems : {s1.DisplayAll()}");
 
             Console.ReadLine();
         }
