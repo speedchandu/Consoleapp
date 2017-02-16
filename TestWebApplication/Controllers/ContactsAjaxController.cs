ee23ed79fe0d241742207eaa903f142777b91161
speedchandu <chandan.bhagat@outlook.com>
2017-02-16 09:00:49 +0545

    new ajax call implemented using ajax.beginform to show create form on bootstrap modal.

--- TestWebApplication/Controllers/ContactsAjaxController.cs
+++ TestWebApplication/Controllers/ContactsAjaxController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
@@ -10,60 +11,24 @@ using TestWebApplication.Models;
 
 namespace TestWebApplication.Controllers
 {
-    public class ContactsController : Controller
+    public class ContactsAjaxController : Controller
     {
         private TestModel_new db = new TestModel_new();
 
-        // GET: Contacts
-        public ActionResult Index(string search="", string field="", bool ascending=true)
+        // GET: ContactsAjax
+        public async Task<ActionResult> Index()
         {
-
-                ViewBag.search = search;
-                ViewBag.ascending = ascending;
-
-                var data = db.Contacts.Where(x => x.Name.Contains(search) || x.Address.Contains(search) || x.Contact_Number.Contains(search) || x.Id.ToString().Contains(search));
-
-                if (field=="name")
-                {
-                    if (ascending)
-                        data = data.OrderBy(p => p.Name);
-                    else
-                        data = data.OrderByDescending(p => p.Name);
-                }
-
-                return View(data);
-
-
+            return View(await db.Contacts.ToListAsync());
         }
 
-        [HttpPost]
-
-        public ActionResult UploadFile(HttpPostedFileBase file)
-        {
-            string[] abc = new string[10];
-            int i = 0;
-            foreach (var item in Request.Form.AllKeys)
-            {
-                abc[i] = Request.Form[item];
-                i++;
-            }
-
-            int j = i;
-
-            file.SaveAs(Server.MapPath("~/Images")+"\\"+ file.FileName);
-
-            return View("Index");
-        }
-
-
-        // GET: Contacts/Details/5
-        public ActionResult Details(int? id)
+        // GET: ContactsAjax/Details/5
+        public async Task<ActionResult> Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Contact contact = db.Contacts.Find(id);
+            Contact contact = await db.Contacts.FindAsync(id);
             if (contact == null)
             {
                 return HttpNotFound();
@@ -71,51 +36,37 @@ namespace TestWebApplication.Controllers
             return View(contact);
         }
 
-        // GET: Contacts/Create
+        // GET: ContactsAjax/Create
         public ActionResult Create()
         {
-            return View();
+            return PartialView();
         }
 
-        // POST: Contacts/Create
+        // POST: ContactsAjax/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name,Contact_Number,Address")] Contact contact)
+        public async Task<string> Create([Bind(Include = "Id,Name,Contact_Number,Address")] Contact contact)
         {
             if (ModelState.IsValid)
             {
                 db.Contacts.Add(contact);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(contact);
-        }
-
-        [HttpPost]
-        //[ValidateAntiForgeryToken]
-        public string Create_New([Bind(Include = "Id,Name,Contact_Number,Address")] Contact contact)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Contacts.Add(contact);
-                db.SaveChanges();
+                await db.SaveChangesAsync();
                 return "Successfully Created";
             }
 
             return "Unable to create";
         }
 
-        // GET: Contacts/Edit/5
-        public ActionResult Edit(int? id)
+        // GET: ContactsAjax/Edit/5
+        public async Task<ActionResult> Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Contact contact = db.Contacts.Find(id);
+            Contact contact = await db.Contacts.FindAsync(id);
             if (contact == null)
             {
                 return HttpNotFound();
@@ -123,30 +74,30 @@ namespace TestWebApplication.Controllers
             return View(contact);
         }
 
-        // POST: Contacts/Edit/5
+        // POST: ContactsAjax/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name,Contact_Number,Address")] Contact contact)
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,Contact_Number,Address")] Contact contact)
         {
             if (ModelState.IsValid)
             {
                 db.Entry(contact).State = EntityState.Modified;
-                db.SaveChanges();
+                await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
             return View(contact);
         }
 
-        // GET: Contacts/Delete/5
-        public ActionResult Delete(int? id)
+        // GET: ContactsAjax/Delete/5
+        public async Task<ActionResult> Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Contact contact = db.Contacts.Find(id);
+            Contact contact = await db.Contacts.FindAsync(id);
             if (contact == null)
             {
                 return HttpNotFound();
@@ -154,14 +105,14 @@ namespace TestWebApplication.Controllers
             return View(contact);
         }
 
-        // POST: Contacts/Delete/5
+        // POST: ContactsAjax/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public async Task<ActionResult> DeleteConfirmed(int id)
         {
-            Contact contact = db.Contacts.Find(id);
+            Contact contact = await db.Contacts.FindAsync(id);
             db.Contacts.Remove(contact);
-            db.SaveChanges();
+            await db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
 
---
+++ TestWebApplication/Models/TestModel_New.cs
@@ -0,0 +1,64 @@
+namespace TestWebApplication.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class TestModel_new : DbContext
+    {
+        // Your context has been configured to use a 'TestModel' connection string from your application's
+        // configuration file (App.config or Web.config). By default, this connection string targets the
+        // 'TestWebApplication.Models.TestModel' database on your LocalDb instance.
+        //
+        // If you wish to target a different database and/or database provider, modify the 'TestModel'
+        // connection string in the application configuration file.
+        public TestModel_new()
+            : base("name=Default1Connection")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        public virtual DbSet<Contact> Contacts { get; set; }
+
+        public virtual DbSet<Category> Categories { get; set; }
+
+
+    }
+
+    public class Contact
+    {
+        [Required(ErrorMessage = "This is required field.")]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Contact_Number { get; set; }
+        public string Address { get; set; }
+    }
+
+    public class Order
+    {
+        public int id { get; set; }
+        [Required]
+        public DateTime OrderDate { get; set; }
+        [Required]
+        public int Product_ID { get; set; }
+        [Required]
+        public int User_ID { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        public int Status { get; set; }
+
+    }
+
+
+    public class Category
+    {
+        public int id { get; set; }
+        public string CategoryName { get; set; }
+        public string Category_desc { get; set; }
+    }
+
+}
